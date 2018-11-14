75511e8ce0a6efd61df37ef3d4c03adad872c672
carpacciao <rezaatv@gmail.com>
2018-11-14 11:50:39 +0100

    exo3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,31 @@
 //menu toggler
+(() => {
+    let menu = document.querySelector('.menu');
+    let navs = menu.querySelectorAll('a');
+    let panels = document.querySelectorAll('.panels > div');
+    
+    for (let i = 0; i < navs.length; i++) {
+        navs[i].addEventListener('click', () => {
+            for (let j = 0; j < navs.length; j++) {
+             navs[j].classList.remove('active');   
+             panels[j].classList.remove('visible');
+            }
+            navs[i].classList.toggle('active');
+            panels[i].classList.toggle('visible');
+    
+        })
+    }
+})();
 
-let menu = document.querySelector('.menu');
-let navs = menu.querySelectorAll('a');
-let panels = document.querySelectorAll('.panels > div');
+//exo 2 name submitter
 
-for (let i = 0; i < navs.length; i++) {
-    navs[i].addEventListener('click', () => {
-        for (let j = 0; j < navs.length; j++) {
-         navs[j].classList.remove('active');   
-         panels[j].classList.remove('visible');
-        }
-        navs[i].classList.toggle('active');
-        panels[i].classList.toggle('visible');
-
-    })
-}
+(() => {
+    let a = document.querySelector('.panels > .name-submitter');
+    let s = a.querySelector('span');
+    let inp = a.querySelector('input');
+    let b = a.querySelector('button');
+    
+    b.addEventListener('click', () => {
+        s.innerText = inp.value;
+    });
+})();
